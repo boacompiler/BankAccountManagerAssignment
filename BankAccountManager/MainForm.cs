2481b34b7eb27754f03f95abc01da17a61d25995
Robert <thegeekiestthingonearth@live.co.uk>
2015-11-24 01:08:26 +0000

    started creating the xml serialiser stalled at taking a generic parameter, but i am tired and ill so this is a problem to sleep on

--- BankAccountManager/MainForm.cs
+++ BankAccountManager/MainForm.cs
@@ -8,6 +8,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using BankAccountManager.Classes;
+
+
+
 
 
 namespace BankAccountManager
---
+++ BankAccountManager/XmlSerialiser/XMLSeriliser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Xml.Serialization;
+
+namespace BankAccountManager.XmlSerialiser
+{
+    class XMLSeriliser
+    {
+
+        private string path;
+        XmlSerializer SerializerObj;
+        //TODO i used the uk speling of serilise which does not mesh with the constant american spellings found in the libraries, i may have to change this
+        public XMLSeriliser()
+        {
+
+        }
+
+    }
+}

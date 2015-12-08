90b2093feae1ba14cc0a7ed5a888a86a68fab3e3
unknown <20183128@U137.college.farnborough>
2015-12-08 15:18:06 +0000

    more testing

--- BankAccountManager/MainForm.cs
+++ BankAccountManager/MainForm.cs
@@ -37,7 +37,7 @@ namespace BankAccountManager
 
             myList = myXML.Deserialise(myList);//TODO remove
 
-            ucm.DisplayControl(menuControl);
+            ucm.DisplayControl(testControl);
         }
     }
 }

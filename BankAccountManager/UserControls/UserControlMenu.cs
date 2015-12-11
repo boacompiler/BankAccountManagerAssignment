9a6db335b78e0dd1207c872a84d2ea5c452e9ccf
unknown <20183128@U278.college.farnborough>
2015-12-11 13:41:14 +0000

    General list view changes

--- BankAccountManager/UserControls/UserControlMenu.cs
+++ BankAccountManager/UserControls/UserControlMenu.cs
@@ -25,29 +25,14 @@ namespace BankAccountManager.UserControls
             listView1.Items.Clear();
             foreach (Account account in MainForm.myList)
             {
-                //ListViewItem item = new ListViewItem(new string[] { account.customerName.GetFullName(), account.Type, "SubItem3", "And so on" });
-                ListViewItem item = new ListViewItem("poop");
+                ListViewItem item = new ListViewItem(new string[] { account.customerName.GetFullName(), account.Type, string.Format("{0:C}",account.AccountBalance.ToString()), "And so on" });//TODO finish
                 listView1.Items.Add(item);
             }
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            FillView();
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
         private void buttonSearch_Click(object sender, EventArgs e)
         {
+            FillView();
             MessageBox.Show(MainForm.myList[0].customerName.GetFullName());
         }
     }

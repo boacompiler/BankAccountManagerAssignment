6dbf7554ee437f49bcf04b76b7d67aff6aa7c182
unknown <20183128@U278.college.farnborough>
2015-12-11 13:18:56 +0000

    my phone number class was untracked and this ruined everything, so here it is

---
+++ BankAccountManager/Classes/PhoneNumber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountManager.Classes
+{
+    //a new class for phonenumbers allows verification to be handled whenever a number is input
+    //the class is rudimentry but has room for expansion
+
+    public class PhoneNumber
+    {
+        private String number;
+
+        public String Number
+        {
+            get 
+            { 
+                return number; 
+            }
+
+            set 
+            {
+                if (value.Length > 11)
+                {
+                    throw new System.ArgumentException("Telephone number too long");
+                }
+                else
+                {
+                    number = value;
+                }
+                 
+            }
+        }
+
+    }
+}
